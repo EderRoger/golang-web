f2997e7168f400b03f50d41c5aa379044aac186b
Eder Roger <eder.roger.souza@gmail.com>
2018-06-11 06:46:32 -0300

    feat: read a template from a file using a text/template package

--- main.go
+++ main.go
@@ -1,27 +1,23 @@
 package main
 
 import (
-	"io"
 	"log"
 	"os"
-	"strings"
+	"text/template"
 )
 
 var name string
 
 func main() {
 
-	name = "Hello World"
-	html := `<html><h2>` + name + `</h2></html>`
-
-	file, err := os.Create("index.html")
+	tpl, err := template.ParseFiles("tpl.gohtml")
 
 	if err != nil {
-		log.Fatal("error creating file", err)
+		log.Fatalln("error creating file", err)
 	}
 
-	defer file.Close()
-
-	io.Copy(file, strings.NewReader(html))
-
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
